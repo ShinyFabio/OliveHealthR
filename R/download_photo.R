626836ad27646a92d083d85dbaf6259fa5fae54f
ShinyFabio <fabiodellarocca94@hotmail.it>
2021-04-02 13:32:41 +0200

    download_photo.R

--- R/download_photo.R
+++ R/download_photo.R
@@ -1,13 +1,12 @@
 #' Download photos and copy in www folder
 #' 
-#' @description This function download photos from a link and copy them in the www folder.
+#' @description This function download photos from a link and copy them in the www folder. Data are uploaded on https://gofile.io/uploadFiles.
 #' 
 #' @usage download_photo()
 #' 
 #' 
 #' 
 #' @param url Url where download the zip file. By default "https://srv-store4.gofile.io/download/Vhtmor/9e840835a48fcd254263fdc03dbf690d".
-#' @param destpath The destination path where to unzip the files. By default ...
 #' 
 #' @export
 #' 
@@ -17,14 +16,12 @@
 
 
 
-# download_photo = function(url = "https://srv-store4.gofile.io/download/Vhtmor/9e840835a48fcd254263fdc03dbf690d", destpath){
-#   
-#   url = paste("https://srv-store4.gofile.io/download/Vhtmor/9e840835a48fcd254263fdc03dbf690d", "imagedata.zip", sep = "/")
-#   temp = tempfile()
-#   download.file(url, temp)
-#   unzip(zipfile = temp, exdir = "C:/Users/fabio/Desktop/www/foo")
-# }
-
-download_photo = function(){
-  base::system.file(package = "OliveHealthR")
+download_photo = function(url = "https://srv-store4.gofile.io/download/Vhtmor/9e840835a48fcd254263fdc03dbf690d"){
+  
+  url = paste("https://srv-store4.gofile.io/download/Vhtmor/9e840835a48fcd254263fdc03dbf690d", "imagedata.zip", sep = "/")
+  temp = tempfile()
+  destpath = base::system.file(package = "OliveHealthR")
+  destpath = paste(destpath, "app/www")
+  download.file(url, temp)
+  unzip(zipfile = temp, exdir = destpath)
 }
