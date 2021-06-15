bb718b60a19f0b1cac2c5d32fb56587f1c940652
ShinyFabio <fabiodellarocca94@hotmail.it>
2021-06-15 09:59:19 +0200

    upd

--- R/zzz.R
+++ R/zzz.R
@@ -7,12 +7,15 @@
 ", "Welcome to OliveHealthR
 ", pkgname, " version ", version, "
 
-Github page: https://github.com/ShinyFabio/OliveHealthR2
+Github page: https://github.com/ShinyFabio/OliveHealthR
 
 If you want to download all the images use download_photo() before launch OliveHealthR.
 
+If you need the data file (.csv) download from this link:
+http://bioinfo.na.iac.cnr.it/olivehealth/File_csv.rar
+
 This message can be suppressed by:
-suppressPackageStartupMessages(library(OliveHealthR2))
+suppressPackageStartupMessages(library(OliveHealthR))
 ========================================
                ")
   
